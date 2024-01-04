9babeba452c4e27d5becfeea1c0936102c131d02
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2024-01-04 11:24:53 -0800

    Display the correct datum numbers.

--- backpropex/trainer.py
+++ backpropex/trainer.py
@@ -43,13 +43,15 @@ class Trainer(TrainProtocol):
     # The loss for the current training item.
     loss: Optional[float] = None
 
-    def __init__(self, network: NetProtocol, /, *, loss_function: LossFunction = MeanSquaredError):
+    def __init__(self, network: NetProtocol, /, *,
+                 loss_function: LossFunction = MeanSquaredError
+                 ):
         self.net = network
         self.loss_function = loss_function
 
     def __call__(self, data: TrainingData, /, *,
                     epochs: int=1, batch_size: int=1,
-                    learning_rate: float=0.1) -> Generator[TrainStepResultAny|InitStepResult, Any, None]:
+                    learning_rate: float=0.1) -> Generator[TrainStepResultAny, Any, None]:
         """
         Train the network on the given training data.
 
@@ -69,7 +71,9 @@ class Trainer(TrainProtocol):
                     input = np.array(input)
                     expected = np.array(expected)
                     with self.training_datum(idx, datum_max, input, expected):
-                        yield from self.train_one(input, expected)
+                        yield from self.train_one(input, expected,
+                                                  datum_number=idx,
+                                                  datum_max=datum_max)
 
 
     @contextmanager
